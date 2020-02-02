b8e579ccb70782bd335563e02aa1bf1fc52d3061
bayuik <58217016+bayuik@users.noreply.github.com>
2020-02-02 15:12:53 +0700

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,32 +1,3 @@
-// // buatlah sebuah function dengan satu parameter string
-// // untuk mengecek apakah string tersebut terdapat huruf vokal
-// // jika iya maka return string jika tidak false (bebas)
-
-// const isStringVowels = (string) => {
-//     // variable penampung soalnya loop javascript aneh
-//     let isVowel = false;
-
-//     // string to lower case untuk case insensitive
-//     let strlowcase = string.toLowerCase();
-
-//     for (let i = 0; i <= strlowcase.length; i++ ) {
-//         if (strlowcase[i] == 'a' || strlowcase[i] == 'i' || strlowcase[i] == 'u' || strlowcase[i] == 'e' || strlowcase[i] == 'o') {
-//             isVowel = true
-//             break; // break karena cuma perlu cek jika contain salah satu vowel aja
-//         }
-//         else {
-//             isVowel = false;
-//         }
-//     }
-//     if(isVowel == true) {
-//         return string;
-//     }
-
-
-// }
-
-// console.log(isStringVowels('WOW'));
-const vokal = (a, b) => {
     let pattern = str.match(/[aiueo]/gi);
   
     if(str) {
